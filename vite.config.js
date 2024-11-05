5c48eb784cb03ffffc6c4f22e422001ad7e141b2
rohitsr0202 <rohitsrcoder@gmail.com>
2024-11-05 10:42:16 +0530

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/your-repo-name/',  // Replace 'your-repo-name' with the name of your GitHub repo
 })
